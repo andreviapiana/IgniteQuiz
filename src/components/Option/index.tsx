8be137acd435abc1ef27c3573250917c74a7a5b1
andreviapiana <andreviapiana@gmail.com>
2023-09-10 19:57:12 -0300

    ✨feat: Feedback Visual - Animando com Skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,12 +1,16 @@
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import {
+  BlurMask,
   Canvas,
   Path,
-  Skia
+  Skia,
+  runTiming,
+  useValue
 } from '@shopify/react-native-skia';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
+import { useEffect } from 'react';
 
 type Props = TouchableOpacityProps & {
   checked: boolean;
@@ -17,10 +21,20 @@ const CHECK_SIZE = 28;
 const CHECK_STROKE = 2;
 
 export function Option({ checked, title, ...rest }: Props) {
+  const percentage = useValue(0);
+  
   const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
 
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
+  useEffect(() => {
+    if(checked) {
+      runTiming(percentage, 1, { duration: 700 })
+    } else {
+      runTiming(percentage, 0, { duration: 700 })
+    }
+  },[checked])
 
   return (
     <TouchableOpacity
@@ -50,8 +64,10 @@ export function Option({ checked, title, ...rest }: Props) {
           style="stroke"
           strokeWidth={CHECK_STROKE}
           start={0}
-          end={0.5}
-        /> 
+          end={percentage}
+        >
+          <BlurMask blur={1} style="solid" />
+        </Path>
       </Canvas>
     </TouchableOpacity>
   );
