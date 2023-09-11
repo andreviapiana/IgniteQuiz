62226a3a9784f82735ca37cbeb9e2ba56a64e86f
andreviapiana <andreviapiana@gmail.com>
2023-09-11 09:59:57 -0300

    ✨feat: Feedback Tátil e Sonoro – Prevenindo Voltar com Gesto no iOS

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,7 +5,7 @@ import { Quiz } from '../screens/Quiz';
 import { Finish } from '../screens/Finish';
 import { History } from '../screens/History';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen, Group } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
@@ -19,17 +19,20 @@ export function AppRoutes() {
         component={Home}
       />
       <Screen
-        name="quiz"
-        component={Quiz}
-      />
-      <Screen
         name="history"
         component={History}
       />
-      <Screen
-        name="finish"
-        component={Finish}
-      />
+      <Group screenOptions={{ gestureEnabled: false }}>
+        <Screen
+          name="quiz"
+          component={Quiz}
+        />
+
+        <Screen
+          name="finish"
+          component={Finish}
+        />
+      </Group>
     </Navigator>
   )
 }
